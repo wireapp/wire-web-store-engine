99724752e95c13745afba6d15ee3c938402157ab
Benny Neugebauer <bn@bennyn.de>
2017-07-04 14:51:27 +0200

    chore: Exporting "ExpirationalStore"

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,12 @@
-import {IndexedDBEngine, LocalStorageEngine} from './engine/index';
+import {ExpirationalStore} from './store';
+import {IndexedDBEngine, LocalStorageEngine} from './engine';
 
 module.exports = {
   engine: {
     IndexedDBEngine,
     LocalStorageEngine,
+  },
+  store: {
+    ExpirationalStore: ExpirationalStore
   }
 };
